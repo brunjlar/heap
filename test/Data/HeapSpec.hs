eb6e6914fff25450514ec570838a6f32ed694d1b
brunjlar <brunjlar@gmail.com>
2016-06-27 23:39:16 +0200

    added test for sorting a longer list

--- test/Data/HeapSpec.hs
+++ test/Data/HeapSpec.hs
@@ -5,6 +5,7 @@ module Data.HeapSpec (spec) where
 import Test.Hspec
 import Data.Foldable (toList)
 import Data.Heap
+import Numeric.Natural
 
 spec :: Spec
 spec = toHeapSpec
@@ -12,7 +13,23 @@ spec = toHeapSpec
 toHeapSpec :: Spec
 toHeapSpec = describe "toHeap" $ do
 
-    it "should create a proper heap" $ do 
+    it "should allow to sort a short list" $ do 
 
         let xs = [(2, 'a'), (5, 'e'), (1, 'H'), (4, 'k'), (7, 'l'), (6, 'l'), (3, 's')]
         toList (toHeap xs) `shouldBe` "Haskell"
+
+    it "should sort allow to sort a long list" $ do
+
+        let n  = 2000
+            xs = [(x, x) | x <- shuffle n]
+        toList (toHeap xs) `shouldBe` [1 .. n]
+
+shuffle :: Natural -> [Natural]
+shuffle n = go [] [1 .. n] where
+
+    go xs ys
+        | null ys   = xs
+        | otherwise = let (x : zs) = take 7 ys
+                          zs'      = drop 7 ys
+                          ys'      = zs' ++ zs
+                      in  go (x : xs) ys'
