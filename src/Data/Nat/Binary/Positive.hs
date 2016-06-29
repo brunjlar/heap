d3caaa4ae51c21211ad15083f09c7b57a919d004
brunjlar <brunjlar@gmail.com>
2016-06-30 00:54:55 +0200

    ordered class

--- src/Data/Nat/Binary/Positive.hs
+++ src/Data/Nat/Binary/Positive.hs
@@ -4,33 +4,17 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE EmptyCase #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Data.Nat.Binary.Positive
-    ( PNat(..)
-    , type (??)
-    , type (<)
-    , type (<=)
-    , type (>=)
-    , type (>)
-    , type Min
-    , SPNat(..)
-    , SPNAT(..)
-    , toSPNAT
-    , toNatural
-    , eqSame
-    , sameEq
-    , decPNat
+    ( Pos(..)
     ) where
 
 import Data.Constraint
 import Data.Logic
-import Numeric.Natural
+import Data.Ordered
 
-data PNat = One | Even PNat | Odd PNat deriving (Show, Read, Eq)
-
-infix 4 ??
+data Pos = One | Even Pos | Odd Pos deriving (Show, Read, Eq)
 
 type family EO (o :: Ordering) :: Ordering where
     EO 'LT = 'LT
@@ -42,132 +26,80 @@ type family OE (o :: Ordering) :: Ordering where
     OE 'EQ = 'GT
     OE 'GT = 'GT
 
-type family (m :: PNat) ?? (n :: PNat) :: Ordering where
-    'One    ?? 'One    = 'EQ
-    'One    ?? _       = 'LT
-    'Even _ ?? 'One    = 'GT
-    'Even m ?? 'Even n = m ?? n
-    'Even m ?? 'Odd n  = EO (m ?? n)
-    'Odd _  ?? 'One    = 'GT
-    'Odd m  ?? 'Even n = OE (m ?? n)
-    'Odd m  ?? 'Odd n  = m ?? n
+type family (m :: Pos) ??? (n :: Pos) :: Ordering where
+    'One    ??? 'One    = 'EQ
+    'One    ??? _       = 'LT
+    'Even _ ??? 'One    = 'GT
+    'Even m ??? 'Even n = m ??? n
+    'Even m ??? 'Odd n  = EO (m ??? n)
+    'Odd _  ??? 'One    = 'GT
+    'Odd m  ??? 'Even n = OE (m ??? n)
+    'Odd m  ??? 'Odd n  = m ??? n
 
-infix 4 <, <=, >=, >
+instance Ordered Pos where 
 
-type family IsLeq (o :: Ordering) :: Bool where
-    IsLeq 'LT = 'True
-    IsLeq 'EQ = 'True
-    IsLeq 'GT = 'False
+    type (??) m n = m ??? n
 
-type (m :: PNat) < (n :: PNat) = (m ?? n) ~ 'LT
+    data Sing Pos n where
 
-type (m :: PNat) <= (n :: PNat) = IsLeq (m ?? n) ~ 'True
+        SOne :: Sing Pos 'One
 
-type (m :: PNat) >= (n :: PNat) = n <= m
+        SEven :: Sing Pos n -> Sing Pos ('Even n)
 
-type (m :: PNat) > (n :: PNat) = n < m
+        SOdd :: Sing Pos n -> Sing Pos ('Odd n)
 
-type Min (m :: PNat) (n :: PNat) = IfThenElse (IsLeq (m ?? n)) m n
+    symm SOne      SOne      = Dict
+    symm SOne      (SEven _) = Dict
+    symm SOne      (SOdd _)  = Dict
+    symm (SEven _) SOne      = Dict
+    symm (SEven m) (SEven n) = using (symm m n) Dict
+    symm (SEven m) (SOdd n)  = using (symm m n) $ case dec m n of
+        DecLT Dict -> Dict
+        DecEQ Dict -> using (sameEq m) Dict
+        DecGT Dict -> using (symm n m) Dict
+    symm (SOdd _)  SOne      = Dict
+    symm (SOdd m)  (SEven n) = using (symm m n) $ case dec m n of
+        DecLT Dict -> Dict
+        DecEQ Dict -> using (sameEq m) Dict
+        DecGT Dict -> using (symm n m) Dict
+    symm (SOdd m)  (SOdd n)  = using (symm m n) Dict
 
-data SPNat :: PNat -> * where
+    eqSame SOne      SOne      = Dict
+    eqSame (SEven m) (SEven n) = using (eqSame m n) Dict
+    eqSame (SEven _) (SOdd _)  = error "impossible branch"
+    eqSame (SOdd _)  (SEven _) = error "impossible branch"
+    eqSame (SOdd m)  (SOdd n)  = using (eqSame m n) Dict
 
-    SOne :: SPNat 'One
+    dec SOne      SOne      = DecEQ Dict
+    dec SOne      (SEven _) = DecLT Dict
+    dec SOne      (SOdd _)  = DecLT Dict
+    dec (SEven _) SOne      = DecGT Dict
+    dec (SEven m) (SEven n) = case dec m n of
+        DecLT Dict -> DecLT Dict
+        DecEQ Dict -> DecEQ Dict
+        DecGT Dict -> DecGT Dict
+    dec (SEven m) (SOdd n) = case dec m n of
+        DecLT Dict -> DecLT Dict
+        DecEQ Dict -> using (sameEq m) $ DecLT Dict
+        DecGT Dict -> DecGT Dict
+    dec (SOdd _)  SOne      = DecGT Dict
+    dec (SOdd m) (SEven n) = case dec m n of
+        DecLT Dict -> DecLT Dict
+        DecEQ Dict -> using (symm m n) $ DecGT Dict
+        DecGT Dict -> DecGT Dict
+    dec (SOdd m) (SOdd n) = case dec m n of
+        DecLT Dict -> DecLT Dict
+        DecEQ Dict -> DecEQ Dict
+        DecGT Dict -> DecGT Dict
 
-    SEven :: SPNat n -> SPNat ('Even n)
+instance Nat Pos where
 
-    SOdd :: SPNat n -> SPNat ('Odd n)
+    toSING 0 = error "zero is not positive"
+    toSING 1 = SING SOne
+    toSING n = case toSING (n `div` 2) of
+        SING n' | even n    -> SING $ SEven n'
+                | otherwise -> SING $ SOdd n'
 
-data SPNAT :: * where
-
-    SPNAT :: SPNat n -> SPNAT
-
-toSPNAT :: Natural -> Maybe SPNAT
-toSPNAT 0 = Nothing
-toSPNAT 1 = Just $ SPNAT SOne
-toSPNAT n = case toSPNAT (n `div` 2) of
-    Just (SPNAT n') | even n    -> Just $ SPNAT $ SEven n'
-                    | otherwise -> Just $ SPNAT $ SOdd n'
-    Nothing                     -> error "impossible branch"
-
-toNatural :: SPNat n -> Natural
-toNatural SOne      = 1
-toNatural (SEven n) = 2 * toNatural n
-toNatural (SOdd n)  = 1 + 2 * toNatural n
-
-instance Show (SPNat n) where
-
-    show = show . toNatural
-
-instance Show SPNAT where
-
-    show (SPNAT n) = show n
-
-data Dec (m :: PNat) (n :: PNat) = DecLT (Dict (m < n)) | DecEQ (Dict (m ~ n)) | DecGT (Dict (m > n))
-    deriving Show
-
-eqSame :: (m ?? n) ~ 'EQ => SPNat m -> SPNat n -> Dict (m ~ n)
-eqSame SOne      SOne      = Dict
-eqSame (SEven m) (SEven n) = using (eqSame m n) Dict
-eqSame (SEven m) (SOdd n)  = case decPNat m n of
-eqSame (SOdd m)  (SEven n) = case decPNat m n of
-eqSame (SOdd m)  (SOdd n)  = using (eqSame m n) Dict
-
-sameEq :: SPNat n -> Dict ((n ?? n) ~ 'EQ)
-sameEq SOne      = Dict 
-sameEq (SEven n) = using (sameEq n) Dict
-sameEq (SOdd n)  = using (sameEq n) Dict
-
-decPNat :: SPNat m -> SPNat n -> Dec m n
-decPNat SOne      SOne      = DecEQ Dict
-decPNat SOne      (SEven _) = DecLT Dict
-decPNat SOne      (SOdd _)  = DecLT Dict
-decPNat (SEven _) SOne      = DecGT Dict
-decPNat (SEven m) (SEven n) = case decPNat m n of
-    DecLT Dict -> DecLT Dict
-    DecEQ Dict -> DecEQ Dict
-    DecGT Dict -> DecGT Dict
-decPNat (SEven m) (SOdd n) = case decPNat m n of
-    DecLT Dict -> DecLT Dict
-    DecEQ Dict -> using (sameEq m) $ DecLT Dict
-    DecGT Dict -> DecGT Dict
-decPNat (SOdd _)  SOne      = DecGT Dict
-decPNat (SOdd m) (SEven n) = case decPNat m n of
-    DecLT Dict -> DecLT Dict
-    DecEQ Dict -> using (sameEq m) $ DecGT Dict
-    DecGT Dict -> DecGT Dict
-decPNat (SOdd m) (SOdd n) = case decPNat m n of
-    DecLT Dict -> DecLT Dict
-    DecEQ Dict -> DecEQ Dict
-    DecGT Dict -> DecGT Dict
-
-{-
-
-leqGeqEq :: (m <= n, m >= n) => SNat m -> SNat n -> Dict (m ~ n)
-leqGeqEq SZ SZ = Dict
-leqGeqEq (SS m) (SS n) = using (leqGeqEq m n) Dict
-
-leqGeqDec :: SNat m -> SNat n -> Either (Dict (m <= n)) (Dict (m >= n))
-leqGeqDec SZ     _      = Left Dict
-leqGeqDec _      SZ     = Right Dict
-leqGeqDec (SS m) (SS n) = leqGeqDec m n
-
-leqGtDec :: SNat m -> SNat n -> Either (Dict (m <= n)) (Dict (m > n))
-leqGtDec SZ     _      = Left Dict
-leqGtDec (SS _) SZ     = Right Dict
-leqGtDec (SS m) (SS n) = leqGtDec m n
-
-geqMin :: (m >= n) => SNat m -> SNat n -> Dict (Min m n ~ n)
-geqMin m n = alternative (leqGtDec m n)
-    (using (leqGeqEq m n) Dict)
-    Dict
-
-minProd :: (l <= m, l <= n) => SNat l -> SNat m -> SNat n -> Dict (l <= Min m n)
-minProd _ m n = alternative (leqGeqDec m n)
-    Dict
-    (using (geqMin m n) Dict)
-
-minSymm :: SNat m -> SNat n -> Dict (Min m n ~ Min n m)
-minSymm m n = alternative (leqGeqDec m n)
-    (using (geqMin n m) Dict)
-    (using (geqMin m n) Dict)
-    -}
+    toNatural SOne      = 1
+    toNatural (SEven n) = 2 * toNatural n
+    toNatural (SOdd n)  = 1 + 2 * toNatural n
---
+++ src/Data/Ordered.hs
@@ -0,0 +1,126 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
+
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
+
+module Data.Ordered
+    ( Ordered(..) 
+    , Nat(..)
+    , SING(..)
+    , Dec(..)
+    , type InvOrd
+    , type (<), type (<=), type (>=), type (>)
+    , type Min
+    , sameEq
+    , leqGeqEq
+    , leqGtDec
+    , leqGeqDec
+    , geqMin
+    , minProd
+    , minSymm
+    ) where
+
+import Data.Constraint
+import Data.Kind 
+import Data.Logic
+import Numeric.Natural
+
+infix 4 ??, <, <=, >, >=
+
+data SING :: * -> * where
+
+    SING :: Sing a n -> SING a
+
+data Dec (m :: a) (n :: a) where
+
+    DecLT :: Dict (m < n) -> Dec m n 
+
+    DecEQ :: Dict ((m ?? n) ~ 'EQ) -> Dec m n 
+
+    DecGT :: Dict (m > n) -> Dec m n 
+
+type family InvOrd (o :: Ordering) :: Ordering where
+
+    InvOrd 'LT = 'GT
+    InvOrd 'EQ = 'EQ
+    InvOrd 'GT = 'LT
+
+class Ordered a where
+
+    type (??) (m :: a) (n :: a) :: Ordering
+    
+    data Sing a :: a -> *
+
+    symm :: Sing a m -> Sing a n -> Dict ((n ?? m) ~ InvOrd (m ?? n))
+
+    eqSame :: (m ?? n) ~ 'EQ => Sing a m -> Sing a n -> Dict (m ~ n)
+
+    dec :: Sing a m -> Sing a n -> Dec m n
+
+class Ordered nat => Nat nat where
+
+    toSING :: Natural -> SING nat
+
+    toNatural :: Sing nat n -> Natural
+
+instance Nat nat => Show (Sing nat n) where
+
+    show = show . toNatural
+
+instance Nat nat => Show (SING nat) where
+
+    show (SING n) = show n
+
+type family IsLeq (o :: Ordering) :: Bool where
+    IsLeq 'LT = 'True
+    IsLeq 'EQ = 'True
+    IsLeq 'GT = 'False
+
+type (m :: a) < (n :: a) = (m ?? n) ~ 'LT
+
+type (m :: a) <= (n :: a) = IsLeq (m ?? n) ~ 'True
+
+type (m :: a) >= (n :: a) = n <= m
+
+type (m :: a) > (n :: a) = n < m
+
+sameEq :: Ordered a => Sing a n -> Dict ((n ?? n) ~ 'EQ)
+sameEq n = using (symm n n) $ case dec n n of
+    DecEQ d -> d
+    _       -> error "impossible branch"
+
+type Min (m :: a) (n :: a) = IfThenElse (IsLeq (m ?? n)) m n
+
+leqGeqEq :: (Ordered a, m <= n, m >= n) => Sing a m -> Sing a n -> Dict (m ~ n)
+leqGeqEq m n = case dec m n of
+    DecEQ Dict -> using (eqSame m n) Dict
+    _          -> error "impossible branch"
+
+leqGtDec :: Ordered a => Sing a m -> Sing a n -> Either (Dict (m <= n)) (Dict (m > n))
+leqGtDec m n = case dec m n of
+    DecLT Dict -> Left Dict
+    DecEQ Dict -> using (sameEq m) $ Left Dict
+    DecGT Dict -> Right Dict
+
+leqGeqDec :: Ordered a => Sing a m -> Sing a n -> Either (Dict (m <= n)) (Dict (m >= n))
+leqGeqDec m n = alternative (leqGtDec m n) (Left Dict) (Right Dict)
+
+geqMin :: (Ordered a, m >= n) => Sing a m -> Sing a n -> Dict (Min m n ~ n)
+geqMin m n = case dec m n of
+    DecEQ Dict -> using (eqSame m n) Dict
+    DecGT Dict -> using (symm n m) Dict
+    _          -> error "impossible branch"
+
+minProd :: (Ordered a, l <= m, l <= n) 
+           => Sing a l -> Sing a m -> Sing a n -> Dict (l <= Min m n)
+minProd _ m n = alternative (leqGeqDec m n)
+    Dict
+    (using (geqMin m n) Dict)
+
+minSymm :: Ordered a => Sing a m -> Sing a n -> Dict (Min m n ~ Min n m)
+minSymm m n = alternative (leqGeqDec m n)
+    (using (geqMin n m) Dict)
+    (using (geqMin m n) Dict)
