3367a7ee090a8d9b8c476e4818299a50188c3f9e
brunjlar <brunjlar@gmail.com>
2016-08-02 23:22:58 +0200

    first proper benchmark

--- benchmark/benchmark.hs
+++ benchmark/benchmark.hs
@@ -1,28 +1,42 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
 import Criterion.Main
+import Data.Foldable     (toList)
 import Data.Heap
+import Data.MyPrelude
+import Data.Nat.Binary   (Bin)
+import Data.Nat.Peano    (Peano)
+import Data.Proxy        (Proxy(..))
 
 main :: IO ()
 main = defaultMain
-    [
+    [ bgroup "Peano" $ map sortBenchEnvPeano ls
+    , bgroup "Bin"   $ map sortBenchEnvBin   ls
     ]
-    {-
-    [ bgroup "white"
-        [ bench "10/200"     $ whnf (w   10)   200
-        , bench "10/2000"    $ whnf (w   10)  2000
-        , bench "10/20000"   $ whnf (w   10) 20000
-        , bench "100/200"    $ whnf (w  100)   200
-        , bench "100/2000"   $ whnf (w  100)  2000
-        , bench "100/20000"  $ whnf (w  100) 20000
-        , bench "1000/200"   $ whnf (w 1000)   200
-        , bench "1000/2000"  $ whnf (w 1000)  2000
-        , bench "1000/20000" $ whnf (w 1000) 20000
-        ]
-    , env setupEnv $ \ ~(m, xss) -> bgroup "linear"
-        [ l m xss  1 5
-        , l m xss  5 5
-        , l m xss 10 5
-        ]
-    ]
-    -}
+
+  where
+
+    ls = [10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120]
+
+sort' :: forall nat. Nat nat => Proxy nat -> [Natural] -> [Natural]
+sort' Proxy = toList . toHeap @nat . map (\x -> (x, x))
+
+sortBench :: Nat nat => Proxy nat -> String -> [Natural] -> Benchmark
+sortBench p name xs = bench name $ nf (sort' p) xs
+
+sortBenchEnv :: Nat nat => Proxy nat -> Natural -> String -> Benchmark
+sortBenchEnv p len name = env (shuffle len) $ sortBench p name
+
+sortBenchEnvPeano :: Natural -> Benchmark
+sortBenchEnvPeano len =
+    sortBenchEnv (Proxy :: Proxy Peano) len $ "Peano-" ++ show len
+
+sortBenchEnvBin :: Natural -> Benchmark
+sortBenchEnvBin len =
+    sortBenchEnv (Proxy :: Proxy Bin) len $ "Bin-" ++ show len
+
+shuffle :: Natural -> IO [Natural]
+shuffle n = return $ map fromIntegral $  evalRand (replicateM (fromIntegral n) $ getRandomR (1 :: Int, 100)) $ mkStdGen 1234567
